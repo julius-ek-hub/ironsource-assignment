919be98ff3866aeccb30be4d1efe54a51fb4f0d4
julius_ek_hub <ekanejulius7@gmail.com>
2022-07-03 14:27:53 +0400

    added handleSelectChange for CheckBoxes against each contact during selection

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -15,16 +15,25 @@ import useContactListingsContext from '../../hooks/useContactsListingsContext';
 
 const Contact = ({ details }) => {
 
-    const { selected } = useContactListingsContext();
+    const { selected, setSelected } = useContactListingsContext();
 
     const { picture, gender, name } = details;
 
-    const isChecked = _id => selected.includes(details._id)
+    const isChecked = _id => selected.includes(details._id);
+
+    const handleSlectChange = (_id) => {
+        if (isChecked(_id)) return setSelected(selected.filter(c => c !== _id));
+        setSelected([...selected, _id]);
+    }
 
     return (
         <ContactDetailsContext.Provider value={details}>
             <ListItem sx={{ bgcolor: isChecked(details._id) ? 'divider' : 'background.paper' }}>
-                {selected.length > 0 && <Checkbox checked={isChecked(details._id)} />}
+                {selected.length > 0 && (
+                    <Checkbox
+                        checked={isChecked(details._id)}
+                        onChange={() => handleSlectChange(details._id)} />
+                )}
                 <ListItemAvatar>
                     <Avatar
                         src={picture.thumbnail}
